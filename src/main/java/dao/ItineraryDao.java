3f63d06d6be6d5fea413a0360ea5739d3d419470
Jungu Kang <wayndw256@gmail.com>
2025-12-05 11:42:51 -0500

    5. Itinerary Done

--- src/main/java/dao/ItineraryDao.java
+++ src/main/java/dao/ItineraryDao.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.*;
 
 import model.Itinerary;
 
@@ -9,26 +10,37 @@ public class ItineraryDao {
 	
 	//Jungu
 	public List<Itinerary> getItineraryForReservation(int resrNo) {
-			/*
-			 * Code to fetch itinerary from resrNo goes here
-			 */
-		
-			List<Itinerary> its = new ArrayList<Itinerary>();
-			
-			for (int i = 0; i < 5; i++) {
-				Itinerary it = new Itinerary();
-				it.setAirlineID("AA");
-				it.setArrival("JFK");
-				it.setDeparture("TNR");
-				it.setArrTime("2011-01-13 23:00:00");
-				it.setDepTime("2011-01-13 06:55:00");
-				it.setFlightNo(111);
-				it.setResrNo(resrNo);
-				
-				its.add(it);			
-			}
-			/*Sample data ends*/
-			
-			return its;
-		}
+	    List<Itinerary> its = new ArrayList<Itinerary>();
+
+	    try {
+	        Class.forName("com.mysql.cj.jdbc.Driver");
+	        Connection con = DriverManager.getConnection(
+	            "jdbc:mysql://localhost:3306/travel_reservation?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true",
+	            "root", "root");
+	        
+	        String sql = "SELECT * FROM Itinerary WHERE ResrNo = ? ORDER BY DepTime ASC";
+	        PreparedStatement pstmt = con.prepareStatement(sql);
+	        pstmt.setInt(1, resrNo);
+	        ResultSet rs = pstmt.executeQuery();
+	        
+	        while (rs.next()) {
+	            Itinerary it = new Itinerary();
+	            it.setResrNo(rs.getInt("ResrNo"));
+	            it.setAirlineID(rs.getString("AirlineID"));
+	            it.setFlightNo(rs.getInt("FlightNo"));
+	            it.setDeparture(rs.getString("Departure"));
+	            it.setArrival(rs.getString("Arrival"));
+	            it.setDepTime(rs.getString("DepTime"));
+	            it.setArrTime(rs.getString("ArrTime"));
+	            it.setActualDepTime(rs.getString("ActualDepTime"));
+	            it.setActualArrTime(rs.getString("ActualArrTime"));
+	            its.add(it);
+	        }
+	        
+	    } catch (Exception e) {
+	        System.out.println(e);
+	    }
+	    
+	    return its;
+	}
 }
